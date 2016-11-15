793abdeae6d2fc395adcac9963e3d5f134bd67ee
jicaey <michaelyoung.dev@gmail.com>
2016-11-15 13:21:14 -0500

    Adds loginVC Textfields and Buttons

--- Gouda0916/Gouda0916/LogInViewController.swift
+++ Gouda0916/Gouda0916/LogInViewController.swift
@@ -10,7 +10,25 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    // Mark: Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var newUserButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // Mark: Buttons
+    @IBAction func loginButtonTouched(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotPasswordButtonTouched(_ sender: UIButton) {
+    }
+    
+    @IBAction func newUseButtonTouched(_ sender: UIButton) {
     }
 }
