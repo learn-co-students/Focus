4d7a3219e7119d56353342c9d3ac08898f1c65a3
Dougly <dgalante11@gmail.com>
2016-11-16 16:30:34 -0500

    create createGoalVC and add text fields

---
+++ Gouda0916/Gouda0916/CreateGoalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateGoalViewController.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/16/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
