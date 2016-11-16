8b6c5f76e66e6fbec16e53b7a96699b87507758b
Dougly <dgalante11@gmail.com>
2016-11-16 17:00:39 -0500

    remove side menu

--- Gouda0916/Gouda0916/MainViewController.swift
+++ Gouda0916/Gouda0916/MainViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
     }
+    
+
 }
