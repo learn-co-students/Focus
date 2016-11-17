9746687b730df3116f0679d16c16dad9101f5ce7
Dougly <dgalante11@gmail.com>
2016-11-17 13:32:22 -0500

    create goal file

---
+++ Gouda0916/Gouda0916/Goal.swift
@@ -0,0 +1,13 @@
+//
+//  Goal.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/17/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import Foundation
+
+class Goal {
+    
+}
