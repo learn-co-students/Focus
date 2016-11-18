2dd5cfcf9d3f5a38b44c6f31b29929d8e5c2b5d2
Dougly <dgalante11@gmail.com>
2016-11-18 09:48:24 -0500

    add DataStore file

---
+++ Gouda0916/Gouda0916/DataStore.swift
@@ -0,0 +1,15 @@
+//
+//  DataStore.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/18/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import Foundation
+
+class DataStore {
+    static let sharedInstance = DataStore()
+    
+    private init() {}
+}
