aee137bd57b0346a5bb4c60b4ceb740c4dffb4fc
Dougly <dgalante11@gmail.com>
2016-11-19 15:43:27 -0500

    add ui elements to customGoalCell

---
+++ Gouda0916/CustomGoalTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomGoalTableViewCell.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/19/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import UIKit
+
+class CustomGoalCell: UITableViewCell {
+    
+    
+    
+}
+
