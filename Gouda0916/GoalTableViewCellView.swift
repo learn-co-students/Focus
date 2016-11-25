051f687e825201465b59ec3f652cd99ddc9b4d2e
Dougly <dgalante11@gmail.com>
2016-11-25 13:04:29 -0500

    made width for progress bars constants rather than multiplier

--- Gouda0916/GoalTableViewCellView.swift
+++ Gouda0916/GoalTableViewCellView.swift
@@ -12,21 +12,39 @@ class GoalTableViewCellView: UIView {
     
     @IBOutlet var contentView: UIView!
     
+   
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var goalLabel: UILabel!
     @IBOutlet weak var allowanceAmountLabel: UILabel!
     @IBOutlet weak var allowanceDescriptionLabel: UILabel!
     
-    @IBOutlet weak var daysGoalView: UIView!
-    @IBOutlet weak var daysProgressView: UIView!
+    @IBOutlet weak var savingGoalView: UIView!
     @IBOutlet weak var daysProgressLabel: UILabel!
-    
-    @IBOutlet weak var savingsGoalView: UIView!
-    @IBOutlet weak var savingsProgressView: UIView!
     @IBOutlet weak var savingsProgressLabel: UILabel!
+    @IBOutlet weak var daysCompleteProgressBarConstraint: NSLayoutConstraint!
+    @IBOutlet weak var savingsProgressBarConstraint: NSLayoutConstraint!
     
     
     
+    
+    func updateLabels() {
+        titleLabel.text = goal.purchasGoal!
+        goalLabel.text = "$\(goal.goalAmount)"
+        allowanceAmountLabel.text = "$\(goal.alloctedDailyBudget)"
+        allowanceDescriptionLabel.text = "daily allowance for \(goal.waysToSaveAsStrings[0])"
+        
+        daysProgressLabel.text = "\(goal.dayCounter)/\(goal.timeframe)"
+        savingsProgressLabel.text = "\(goal.currentAmountSaved)/\(goal.goalAmount)"
+        
+        
+    }
+    
+    var goal: Goal! {
+        didSet {
+            updateLabels()
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,26 +55,18 @@ class GoalTableViewCellView: UIView {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
-    var goal: Goal! {
-        didSet {
-            
-        }
-    }
 
     func commonInit() {
-        
         Bundle.main.loadNibNamed("GoalTableViewCellView", owner: self, options: nil)
-        
         self.addSubview(contentView)
-        
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        
         contentView.topAnchor.constraint(equalTo: self.topAnchor)
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         contentView.rightAnchor.constraint(equalTo: self.rightAnchor)
         contentView.leftAnchor.constraint(equalTo: self.leftAnchor)
     }
+    
+    
 
 }
 
