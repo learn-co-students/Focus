e6adfd2c48d7d522c53d95c2a9d1b1f1a85c35c1
Dougly <dgalante11@gmail.com>
2016-11-25 16:19:54 -0500

    new xib for creating goals

---
+++ Gouda0916/CreateGoalStepView.swift
@@ -0,0 +1,38 @@
+//
+//  CreateGoalStepView.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/25/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalStepView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        
+        Bundle.main.loadNibNamed("CreateGoalStepView", owner: self, options: nil)
+        
+        self.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        
+    }
+    
+}
