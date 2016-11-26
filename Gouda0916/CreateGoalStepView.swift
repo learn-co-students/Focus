72222ec87b2c9d60f226d0efa97ece67d1ab7726
Dougly <dgalante11@gmail.com>
2016-11-26 14:23:45 -0500

    scroll view set up

--- Gouda0916/CreateGoalStepView.swift
+++ Gouda0916/CreateGoalStepView.swift
@@ -11,6 +11,8 @@ import UIKit
 class CreateGoalStepView: UIView {
     
     @IBOutlet var contentView: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
---
+++ Gouda0916/ScrollViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ScrollViewController.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/26/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+    
+    @IBOutlet weak var purchaseGoal: CreateGoalStepView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        purchaseGoal.textField.placeholder = "Helllooo"
+        
+        
+    }
+}
