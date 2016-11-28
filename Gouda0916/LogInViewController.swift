89cfd9014f02cecbb555421f74d2846d709db743
Victoria Melendez <vamelend@gmail.com>
2016-11-28 11:06:49 -0500

    updates

--- Gouda0916/LogInViewController.swift
+++ Gouda0916/LogInViewController.swift
@@ -17,26 +17,92 @@ class LogInViewController: UIViewController {
     let store = DataStore.sharedInstance
     
     // Mark: Constants
-    let loginToMain = "loginToMain"
-    
+    //    let loginToMain = "loginToMain"
+    var user1 = DataStore.sharedInstance.userName
+    var emailPopulated = false
+    var passwordPopulated = false
     // Mark: Outlets
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var forgotPasswordButton: UIButton!
     @IBOutlet weak var newUserButton: UIButton!
-
+    
     override func viewDidLoad(){
         super.viewDidLoad()
-       
+        
+        //set tags
+        self.emailTextField.tag = 100
+        self.passwordTextField.tag = 101
+        
+        //set delegates
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        //add layout constraint function
+        self.emailPopulated = false
+        self.passwordPopulated = false
+        
         passwordTextField.isSecureTextEntry = true
         
-        FIRAuth.auth()!.addStateDidChangeListener() { auth, user in
-            if user != nil {
-                 self.performSegue(withIdentifier: self.loginToMain, sender: nil)
+        //        print("** inside viewDidLoad(), self.emailPopulated: \(self.emailPopulated), self.passwordPopulated: \(self.passwordPopulated)")
+        
+        
+        
+        FIRAuth.auth()!.addStateDidChangeListener() { auth, authenticatedEmail in
+            if authenticatedEmail != nil {
+                guard let uid = authenticatedEmail?.uid else {return}
             }
+                
+                
+            else if self.emailPopulated || self.passwordPopulated {
+                print("*** else if self.emailPopulated || self.passwordPopulated in FIRAuth.auth()!.addStateDidChangeListener(), authenticatedEmail = \(authenticatedEmail) ***")
+            }
+                
+            else {  // enters here when authenticatedEmail = nil
+                print("*** else in FIRAuth.auth()!.addStateDidChangeListener(), authenticatedEmail = \(authenticatedEmail) ***")
+            }
+            
+        }
+        
+    }// end of view did load
+    
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        /// to check if email & password are longer than however many characters
+        
+        switch emailTextField.tag {
+        case 100, 101:   // check to see if email has a value
+            if self.emailTextField.text!.utf16.count > 0 && self.passwordTextField.text!.utf16.count > 0 {
+                self.emailPopulated = true
+                self.passwordPopulated = true
+                print ("HELLLLLLLLLLLLLOOOOOOOO")
+            }
+            else {
+                self.emailPopulated = false
+            }
+            self.enableDisableSignIn()
+        default: break
+        }
+        return true
+    }
+    
+    
+    
+    
+    func enableDisableSignIn(){
+        if self.emailPopulated && self.passwordPopulated {
+            self.loginButton.isEnabled = true
+            self.loginButton.becomeFirstResponder()
+            self.loginButton.alpha = 1.0
+        } else {
+            self.loginButton.isEnabled = false
+            self.loginButton.alpha = 0.3
         }
     }
+    
     
     func handleSignIn() {
         guard let email = emailTextField.text, let password = passwordTextField.text else { return }
@@ -57,11 +123,33 @@ class LogInViewController: UIViewController {
         })
     }
     
+    
+    
     //Mark: Buttons
     @IBAction func loginButtonTouched(_ sender: AnyObject) {
-        print("something obvious, but i dont know what")
-        FIRAuth.auth()!.signIn(withEmail: emailTextField.text!,
-                               password: passwordTextField.text!)
+        if let email = self.emailTextField.text {
+            if let password = self.passwordTextField.text {
+                
+                self.user1.email = email
+                
+                FIRAuth.auth()!.signIn(withEmail: email, password: password) { (user, error) in
+                    if error == nil {
+                        self.performSegue(withIdentifier: "loginSegue", sender: self.loginButton)
+                    }
+                    else if error != nil {
+                        let nserror = error as! NSError
+                        let errorvalue = nserror.userInfo["error_name"] as! String
+                        if errorvalue == "ERROR_INVALID_EMAIL" || errorvalue == "ERROR_USER_NOT_FOUND" {
+                            self.indicateError(fieldName: self.emailTextField)
+                        } else if errorvalue == "ERROR_WRONG_PASSWORD" {
+                            self.indicateError(fieldName: self.passwordTextField)
+                        } else {
+                            print("onClickSignIn: Another type of error was returned: \(errorvalue). This error needs to be handled.")
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func forgotPasswordButtonTouched(_ sender: UIButton) {
@@ -115,6 +203,35 @@ class LogInViewController: UIViewController {
         alert.addAction(cancelAction)
         present(alert, animated: true, completion: nil)
     }
+    
+    
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "loginSegue") {
+            let destinationViewController = segue.destination as! MainViewController
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool { return true }
+    
+    func indicateError(fieldName textFieldWithError: UITextField){
+        UIView.animate(withDuration: 1, animations: {
+            textFieldWithError.backgroundColor = UIColor.red
+            textFieldWithError.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: { success in
+            UIView.animate(withDuration: 1, animations: {  // reset control to original state
+                textFieldWithError.backgroundColor = UIColor.white
+                textFieldWithError.transform = CGAffineTransform(scaleX: 1.0, y:1.0)
+            })
+        })
+        self.loginButton.isEnabled = false
+        self.loginButton.alpha = 0.3
+        
+    }
+    
+    
 }
 
 
@@ -129,6 +246,7 @@ extension LogInViewController: UITextFieldDelegate {
         return true
     }
 }
+
 
 
 
