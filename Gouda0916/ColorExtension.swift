9d9ff85e896571426497a453181170025ce0f7fa
Victoria Melendez <vamelend@gmail.com>
2016-11-28 11:56:55 -0500

    Color Extension

---
+++ Gouda0916/ColorExtension.swift
@@ -0,0 +1,39 @@
+//
+//  ColorExtension.swift
+//  Gouda0916
+//
+//  Created by Victoria Melendez on 11/28/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static var themeDarkGreenColor: UIColor {
+        return UIColor(colorLiteralRed: 0/255, green: 121/255, blue: 107/255, alpha: 1)
+    }
+    static var themeLightGreenColor: UIColor {
+        return UIColor(colorLiteralRed: 0/255, green: 150/255, blue: 136/255, alpha: 1)
+    }
+    static var themeAccentGoldColor: UIColor {
+        return UIColor(colorLiteralRed: 255/255, green: 193/255, blue: 7/255, alpha: 1)
+    }
+    static var themeLightPrimaryBlueColor: UIColor {
+        return UIColor(colorLiteralRed: 178/255, green: 223/255, blue: 219/255, alpha: 1)
+    }
+    static var themeTextIconsWhiteColor: UIColor {
+        return UIColor(colorLiteralRed: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    }
+    static var themeBlackColor: UIColor {
+        return UIColor(colorLiteralRed: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+    }
+    static var themeDarkGrayColor: UIColor {
+        return UIColor(colorLiteralRed: 117/255, green: 117/255, blue: 117/255, alpha: 1)
+    }
+    static var themeLightGrayColor: UIColor {
+        return UIColor(colorLiteralRed: 189/255, green: 189/255, blue: 189/255, alpha: 1)
+    }
+    
+}
