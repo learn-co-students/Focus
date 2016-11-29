efd0f7e146f9c19fec5b6fa6dddc8b2de3db4b47
Dougly <dgalante11@gmail.com>
2016-11-29 12:23:17 -0500

    inits set up for userInputView class

--- Gouda0916/UserInputView.swift
+++ Gouda0916/UserInputView.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2016 Flatiron. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UserInputView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("UserInputView", owner: self, options: nil)
+        self.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+    }
+    
+    
+    
+}
+
