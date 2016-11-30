f0b785825e78401c0ca51e177e16b8bb934ba9a6
Dougly <dgalante11@gmail.com>
2016-11-30 18:44:44 -0500

    added vc for editing a goal

---
+++ Gouda0916/EditGoalViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EditGoalViewController.swift
+//  Gouda0916
+//
+//  Created by Douglas Galante on 11/30/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditGoalViewController: UIViewController {
+    
+    @IBOutlet weak var optionsCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension EditGoalViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = optionsCollectionView.dequeueReusableCell(withReuseIdentifier: "editGoalCell", for: indexPath) as! EditGoalCustomCell
+        cell.backgroundColor = UIColor.themeAccentGoldColor
+        return cell
+    }
+    
+}
+
+class EditGoalCustomCell: UICollectionViewCell {
+    
+    
+}
