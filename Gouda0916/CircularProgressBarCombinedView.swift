60410f0aafb369de984520d29861cddb2b413d6f
Victoria Melendez <vamelend@gmail.com>
2016-12-01 15:23:08 -0500

    more Code

---
+++ Gouda0916/CircularProgressBarCombinedView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressBarCombinedView.swift
+//  Gouda0916
+//
+//  Created by Victoria Melendez on 12/1/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+import UIKit
+
+
+class CircularProgressBarCombinedView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    CircularProgressCombined.drawProgressVelocityCompilation()
+        
+    
+    }
+}
