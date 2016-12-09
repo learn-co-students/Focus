32859cb64376851bf1d31bba0d658718b212c587
Dougly <dgalante11@gmail.com>
2016-12-08 21:06:19 -0500

    added functionality to handle user daily input

--- Gouda0916/UserInputProtocol.swift
+++ Gouda0916/UserInputProtocol.swift
@@ -9,36 +9,50 @@
 import Foundation
 
 protocol UserInputProtocol {
-    func incrementDayAndAmount(goal: Goal, textField: UITextField) -> Bool
-    func checkStatus(goal: Goal)
-    func completeGoal(goal: Goal)
+    var store: DataStore { get }
+    func checkForVelocity(goal: Goal, textField: UITextField) -> Bool
+    func incrementDayAndAmount(goal: Goal, textField: UITextField)
+    func checkIfComplete(goal: Goal)
 }
 
 extension UserInputProtocol {
     
-    //updates goal stats based on user input
-    func incrementDayAmount(goal: Goal, textField: UITextField) -> Bool {
+    //returns true if user spent less than allocaed budget that day
+    func checkForVelocity(goal: Goal, textField: UITextField) -> Bool {
         var stayedUnderBudget = false
         if let userInput = textField.text {
             if let amountSpent = Double(userInput) {
                 if amountSpent <= goal.dailyBudget {
                     stayedUnderBudget = true
                 }
-                goal.currentAmountSaved += amountSpent
             }
         }
-        
         return stayedUnderBudget
     }
     
-    //checks if the user saved enough for velocity and if the goal is complete
-    func checkStatusAndVelocity(goal: Goal) -> Bool {
-      return true
+    //updates goal stats based on user input
+    func incrementDayAmount(goal: Goal, textField: UITextField) {
+        if let userInput = textField.text {
+            if let amountSpent = Double(userInput) {
+                goal.currentAmountSaved += amountSpent
+                goal.dayCounter += 1
+            }
+        }
     }
     
-    //goal is removed and new goal is made active or user is asked to create a new goal
-    func completeGoal(goal: Goal) {
     
+    //checks if the goal is complete, if it is it removes it and sets a new goal
+    func checkStatus(goal: Goal, notify: (Goal, Bool) -> Void) {
+        if goal.currentAmountSaved >= goal.goalAmount {
+            notify(goal, true)
+            store.goals.remove(at: 0)
+            if let nextGoal = store.goals.first {
+                nextGoal.isActiveGoal = true
+            } else {
+                print("no queued goals")
+            }
+        }
     }
+    
     
 }
